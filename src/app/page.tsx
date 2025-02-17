b765d6f4e928a74be9a98f1c2332ec57fff68f2b
Ankush307 <ankushdhull966@gmail.com>
2025-02-17 16:57:13 +0530

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <>
       <div className="flex justify-center items-center gap-3 py-10">
-        <Link href={"/question-1/dashboard"} className="">Question-1</Link>
-        <Link href={"/question-2/dashboard"} className="">Question-2</Link>
+        <Link href={"/question-1/dashboard"} className="bg-black text-white py-3 rounded-lg px-6 hover:bg-gray-200 hover:text-black transition-all duration-300 border border-black font-bold">Question-1</Link>
+        <Link href={"/question-2/dashboard"} className="bg-black text-white py-3 rounded-lg px-6 hover:bg-gray-200 hover:text-black transition-all duration-300 border border-black font-bold">Question-2</Link>
       </div>
     </>
   );
